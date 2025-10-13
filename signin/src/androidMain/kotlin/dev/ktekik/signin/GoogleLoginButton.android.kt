dd7ef2cd33b84ddb6eace2ecc176034e5090fb16
atekik <ahmettekik@icloud.com>
2025-10-12 21:51:10 -0400

    moving things around to fix errors

--- signin/src/androidMain/kotlin/dev/ktekik/signin/GoogleLoginButton.android.kt
+++ signin/src/androidMain/kotlin/dev/ktekik/signin/GoogleLoginButton.android.kt
@@ -5,38 +5,11 @@ import androidx.activity.result.contract.ActivityResultContract
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.common.api.ApiException
 import dev.ktekik.signin.di.SIGN_IN_REQUEST_CODE
 import dev.ktekik.signin.di.SignInResult
-import dev.ktekik.signin.models.GoogleAccount
-import dev.ktekik.signin.models.Profile
 import org.koin.androidx.compose.koinViewModel
 import org.koin.compose.koinInject
 import org.orbitmvi.orbit.compose.collectAsState
-
-val GoogleSignInAccount.googleAccount: GoogleAccount
-    get() = GoogleAccount(
-        idToken = idToken.orEmpty(),
-        accessToken = serverAuthCode.orEmpty(),
-        profile = Profile(
-            name = displayName.orEmpty(),
-            familyName = familyName.orEmpty(),
-            givenName = givenName.orEmpty(),
-            email = email.orEmpty(),
-            picture = photoUrl?.toString().orEmpty()
-        ),
-    )
-
-val ApiException.fullErrorMessage: String
-    get() {
-        return listOfNotNull(
-            "code: $statusCode",
-            message?.let { "message: $message" },
-            "localizedMessage: $localizedMessage",
-            "status: $status"
-        ).joinToString("\n")
-    }
 
 @Composable
 internal actual fun GoogleLoginButton(
